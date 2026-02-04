027ca89dca5120a7e0ef29b34ff79adb7b68f6e6
lakshmich0166 <lakshmich0166@gmail.com>
2026-02-04 16:42:37 +0530

    3rd time

--- Check_Odd_Or_Even.js
+++ Check_Odd_Or_Even.js
@@ -5,3 +5,10 @@ if (num % 2 === 0) {
 } else {
   console.log("Odd");
 }
+
+function isPalindrome(str) {
+  let reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam"));
